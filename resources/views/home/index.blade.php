a435c91e1cbec2a6350fb91bd75b7d0ab4337000
Alberto Cid <albertocidandrade@gmail.com>
2022-04-08 01:57:11 +0200

    estructura

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,16 +11,71 @@
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 </head>
 <body>
-    <h1>home</h1>
-    @foreach($places as $place)
-        <div class="card">
-            <img src="{{$place -> place_img}}" alt="">
-            <div>
-                <h2>{{$place -> place_title}}</h2>
-                <p><i class="uil uil-map-marker"></i><a href="https://goo.gl/maps/DPLZUp7RGxfpQ6qM6" target="_BLANK">{{$place -> place_city}}</a></p>
-                <p>{{$place -> place_description}}</p>
-            </div>
+    <div id="wrapper">
+
+        <header>
+
+            <button id="hamburger"> <!-- botón hamburguesa del menu mobile -->
+                <div class="bar"></div>
+            </button>
+
+            <a href="/home" id="logo">City<span>Point</span></a>
+
+            <img src="/assets/userPlaceholderImg.png" alt="User Pic" id="userPic">
+
+        </header>
+        <div id="content">
+
+            <main>
+    
+                <section id="title">
+                    <h2>Explore</h2>
+                    <p>What do you want to discover?</p>
+                </section>
+    
+                <section id="categories">
+                    <button id="categoriesMonument">Monument</button>
+                    <button id="categoriesNature">Nature</button>
+                    <button id="categoriesFood">Food</button>
+                    <button id="categoriesSecret">Secret</button>
+                </section>
+    
+                <section id="cities">
+                    <div class="city">Sevilla</div>
+                    <div class="city">Madrid</div>
+                    <div class="city">Valencia</div>
+                    <div class="city">Lisboa</div>
+                </section>
+    
+                <section id="random">
+                <h2>Random Places</h2>
+    
+                    @foreach($places as $place)
+                        <div class="card">
+                            <img src="{{$place -> place_img}}" alt="">
+                            <div>
+                                <h2>{{$place -> place_title}}</h2>
+                                <p><i class="uil uil-map-marker"></i><a href="https://goo.gl/maps/DPLZUp7RGxfpQ6qM6" target="_BLANK">{{$place -> place_city}}</a></p>
+                                <p>{{$place -> place_description}}</p>
+                            </div>
+                        </div>
+                    @endforeach
+    
+                </section>
+    
+            
+    
+            </main>
+    
+            <aside id="favorites">
+                <h2>Favorite Places</h2>
+            </aside>
         </div>
-    @endforeach
+
+        <footer></footer>
+
+        
+
+    </div>
 </body>
 </html>

d6cbe255cba56a32efeefc9cd29afd9e17c9db7c
Alberto Cid <albertocidandrade@gmail.com>
2022-04-16 14:39:52 +0200

    added fav functionallity to all pages

---
+++ public/filter.js
@@ -0,0 +1,71 @@
+var clicked_btn
+
+        $(document).on('click', '.fav_btn', function (){
+            let id = $(this).val()
+            clicked_btn = $(this)
+            
+            $(this).addClass('btn_active')
+            $(this).children('i').addClass('fav_active')
+                        
+            
+            $.ajax({
+                method: 'GET',
+                dataType: 'json',
+                url: url_global + '/add_fav',
+                data: {
+                    '_token': token,
+                    'place_id': $(this).val()
+                },
+                success: function(response) {
+
+                    if(response == 1){
+                        
+                        $('#content').append(
+                            '<div id="alert">'+
+                                '<div>'+
+                                    '<p>This place is already in your favorites list, do you want to remove it from the list?</p>'+
+                                    '<div>'+
+                                        '<button id="btn_confirm_alert" value="'+ id +'">Yes</button>'+
+                                        '<button id="btn_cancel_alert">No</button>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'
+                        )
+                        
+                    }else {
+
+                        console.log('añadido');
+                        
+                    }
+                }
+            })
+        });
+
+        $(document).on('click', '#btn_cancel_alert', function() {
+            $('#alert').remove();
+        })
+
+        $(document).on('click', '#btn_confirm_alert', function() {
+
+            clicked_btn.removeClass('btn_active')
+            clicked_btn.children('i').removeClass('fav_active')
+
+            $.ajax({
+                method: 'GET',
+                dataType: 'json',
+                url: url_global + '/delete_fav',
+                data: {
+                    '_token': token,
+                    'place_id': $('#btn_confirm_alert').val()
+                },
+                success: function(response) {
+
+
+                    console.log('borrado');
+
+                }
+            })
+
+            $('#alert').remove();
+            
+        })

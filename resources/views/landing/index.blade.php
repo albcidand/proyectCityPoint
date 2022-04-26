d74cd6f1869cbb93ee9cab91b8613c62df5d722c
Alberto Cid <albertocidandrade@gmail.com>
2022-04-26 02:29:56 +0200

    landing design

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -15,29 +15,58 @@
 <body>
     <div class="wrapper">
 
-        <header></header>
+        <header>
+        <div id="logo">City<span>Point</span></div>
+        <ul id="nav">
+            <li><button>Login</button></li>
+            <li><button>Sign Up</button></li>
+        </ul>
+        </header>
 
         <main>
-            <section class="presentation">
-
-            </section>
-
-            <section class="login">
-
-                <div class="tarjeta blue">
-                    <h1>City<span>Point</span></h1>
-                    <form id="loginForm" action="" method="post">
-                        <input id="email" type="email" name="email" placeholder="Email">
-                        <input type="password" id="password" name="password" placeholder="Password">
-                        <button id="loginBtn">Login</button>
-                    </form>
-                    <div id="guestContainer">
-                        <p>Or</p>
-                        <a id="guestBtn" href="/home">Enter as a Guest</a>
+            <div id="presentation">
+                <section id="p_text">
+                    <div>
+                        <i class="fa-solid fa-map-location-dot"></i>
                     </div>
-                </div>
+                    <div>
+                        <h1>Discover <br><span>Unique</span> Places</h1>
+                        <p>A personal city guide in your mobile.</p>
+                    </div>
 
-            </section>
+                    <div>
+                        <img src="/assets/play.png" alt="google play">
+                        <img src="/assets/apple.svg" alt="apple store">
+                    </div>
+
+                </section>
+                <section id="mockup">
+                    
+                </section>
+            </div>
+            
+            <div id="description">
+                <section class="login">
+                    
+                    <div class="tarjeta blue">
+                        <h1>City<span>Point</span></h1>
+                        <form id="loginForm" action="" method="post">
+                            <input id="email" type="email" name="email" placeholder="Email">
+                            <input type="password" id="password" name="password" placeholder="Password">
+                            <button id="loginBtn">Login</button>
+                        </form>
+                        <div id="guestContainer">
+                            <p>Or</p>
+                            <a id="guestBtn" href="/home">Enter as a Guest</a>
+                        </div>
+                    </div>
+    
+                </section>
+
+                <section id="list">
+
+                </section>
+            </div>
 
         </main>
 

e29536c760f8ae909bdc03268b3975fa023f3f52
Alberto Cid <albertocidandrade@gmail.com>
2022-05-04 00:00:36 +0200

    translate = no

--- resources/views/favorites/index.blade.php
+++ resources/views/favorites/index.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" translate="no">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Favorites</title>
     <link rel="stylesheet" href="css/commonStyles.css">
     <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
